6d565fc9b1340ad6aa84ca8179d5b07e298c9618
vincP <vincent_31620@hotmail.fr>
2015-10-21 09:16:13 +0200

    Fichier de Test commit

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once('Model/connexion_sql.php');
+
+if (!isset($_GET['section']) OR $_GET['section'] == 'connexion')
+{
+    include_once('Controler/connexion/connexion.php');
+}
+
+if(isset($_GET['section']) AND $_GET['section'] == 'inscription')
+{
+	include_once('Controler/inscription/inscription.php');
+};
+
+if(isset($_GET['section']) AND $_GET['section'] == 'magasin')
+{
+	include_once('Controler/magasin/magasin.php');
+}
+
+if(isset($_GET['section']) AND $_GET['section'] == 'detailProduit')
+{
+	include_once('Controler/magasin/detailProduit.php');
+}
+
+if(isset($_GET['section']) AND $_GET['section'] == 'achat')
+{
+	include_once('Controler/magasin/achat.php');
+}
